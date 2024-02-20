ad69ed524d79842120f07d0322a42bf8a0475f97
Michael Farwell <whitewolf9909@gmail.com>
2024-02-20 12:16:08 -0500

    complete 1-3

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,12 +1,14 @@
 <script
     setup
     lang="ts">
-
+const $route = useRoute();
 </script>
 
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
+  <p>Chapter slug: {{$route.params.chapterSlug}}</p>
+  <p>Lesson slug: {{$route.params.lessonSlug}}</p>
 </template>
 
 <style scoped>
