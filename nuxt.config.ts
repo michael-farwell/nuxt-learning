d2c60a59f07c3e3e2e2cec4e7eead88cd85fa82f
Michael Farwell <whitewolf9909@gmail.com>
2024-02-20 11:57:21 -0500

    complete 1-1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
   devServer: {
     port: 5173,
   },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
