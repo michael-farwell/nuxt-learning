ab89ab2d21211fbb9f8167c0ce6aa5b8a555d8f2
Michael Farwell <whitewolf9909@gmail.com>
2024-02-20 12:25:28 -0500

    complete 1-5

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -2,13 +2,17 @@
     setup
     lang="ts">
 const $route = useRoute();
+const course = useCourse();
+
+const chapter = computed(() => course.chapters.find((chapter) => chapter.slug === $route.params.chapterSlug));
+const lesson = computed(() => chapter.value!.lessons.find((lesson) => lesson.slug === $route.params.lessonSlug));
 </script>
 
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter slug: {{$route.params.chapterSlug}}</p>
-  <p>Lesson slug: {{$route.params.lessonSlug}}</p>
+  <p>{{chapter.title}}</p>
+  <p>{{lesson.title}}</p>
 </template>
 
 <style scoped>
