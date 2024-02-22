2b2c648ce73de1a1aed67bbadb24056145dfd88a
Michael Farwell <whitewolf9909@gmail.com>
2024-02-22 06:49:00 -0500

    complete 4-3

---
+++ middleware/auth.ts
@@ -0,0 +1,4 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  if (to.params.chapterSlug === "1-chapter-1") return;
+  return navigateTo("/login");
+});
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -5,27 +5,30 @@ const $route = useRoute();
 const course = useCourse();
 
 definePageMeta({
-  middleware: function({ params }, from) {
-    const course = useCourse();
-    const chapter = course.chapters.find((chapter) => chapter.slug === params.chapterSlug);
-    if (!chapter) {
-      return abortNavigation(
-          createError({
-            statusCode: 404,
-            message: "Chapter not found",
-          }),
-      );
-    }
-    const lesson = chapter.lessons.find((lesson) => lesson.slug === params.lessonSlug);
-    if (!lesson) {
-      return abortNavigation(
-          createError({
-            statusCode: 404,
-            message: "Lesson not found",
-          }),
-      );
-    }
-  },
+  middleware: [
+    function({ params }, from) {
+      const course = useCourse();
+      const chapter = course.chapters.find((chapter) => chapter.slug === params.chapterSlug);
+      if (!chapter) {
+        return abortNavigation(
+            createError({
+              statusCode: 404,
+              message: "Chapter not found",
+            }),
+        );
+      }
+      const lesson = chapter.lessons.find((lesson) => lesson.slug === params.lessonSlug);
+      if (!lesson) {
+        return abortNavigation(
+            createError({
+              statusCode: 404,
+              message: "Lesson not found",
+            }),
+        );
+      }
+    },
+    "auth",
+  ],
 });
 
 const chapter = computed(() => course.chapters.find((chapter) => chapter.slug === $route.params.chapterSlug));
