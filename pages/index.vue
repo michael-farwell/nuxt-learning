06b2fdd4b2f2ac9a06bf20b8fcf612b34be16db2
Michael Farwell <whitewolf9909@gmail.com>
2024-02-22 06:15:25 -0500

    complete 4-2

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,13 @@
-<template>Hello World!</template>
+<script
+    setup
+    lang="ts">
+const { path } = useFirstLesson();
+</script>
+
+<template>
+  <NuxtLink
+      class="underline font-medium"
+      :to="path">
+    Go to first lesson
+  </NuxtLink>
+</template>
