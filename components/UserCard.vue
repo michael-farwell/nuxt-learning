6073bb515f2fcda53e9ba978dd83acc62442eb71
Michael Farwell <whitewolf9909@gmail.com>
2024-03-01 06:51:41 -0500

    complete 4-6

---
+++ components/UserCard.vue
@@ -0,0 +1,29 @@
+<script
+    setup
+    lang="ts">
+const user = useSupabaseUser();
+
+const name = computed((): string => user.value?.user_metadata.full_name || "");
+const profile = computed((): string => user.value?.user_metadata.avatar_url || "");
+</script>
+
+<template>
+  <div
+      v-if="user"
+      class="rounded p-3 flex items-center space-x-3 bg-white">
+    <img
+        class="rounded-full size-12 border-2 border-blue-400"
+        :src="profile"
+        :alt="`${name} Profile Avatar`">
+    <div class="text-right">
+      <div class="font-medium">{{name}}</div>
+      <button class="text-sm underline text-slate-500">
+        Log out
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
