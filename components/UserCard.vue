6daaa307d21bf7be12c160f29f603e89a160277e
Michael Farwell <whitewolf9909@gmail.com>
2024-03-01 06:56:32 -0500

    complete 4-7

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -1,10 +1,20 @@
 <script
     setup
     lang="ts">
+const { auth } = useSupabaseClient();
 const user = useSupabaseUser();
 
 const name = computed((): string => user.value?.user_metadata.full_name || "");
 const profile = computed((): string => user.value?.user_metadata.avatar_url || "");
+
+const logout = async () => {
+  const { error } = await auth.signOut();
+  if (error) {
+    console.error(error);
+    return;
+  }
+  await navigateTo("/login");
+};
 </script>
 
 <template>
@@ -17,7 +27,9 @@ const profile = computed((): string => user.value?.user_metadata.avatar_url || "
         :alt="`${name} Profile Avatar`">
     <div class="text-right">
       <div class="font-medium">{{name}}</div>
-      <button class="text-sm underline text-slate-500">
+      <button
+          class="text-sm underline text-slate-500"
+          @click="logout">
         Log out
       </button>
     </div>
