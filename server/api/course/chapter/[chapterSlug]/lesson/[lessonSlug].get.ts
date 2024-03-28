bb3af1ccd71ce4d06721ee9b6aa0ee3932cb64d6
Michael Farwell <whitewolf9909@gmail.com>
2024-03-28 06:43:42 -0400

    complete 5-12

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,22 +1,17 @@
-import course from "~/server/courseData";
-import { Chapter, Lesson, LessonWithPath } from "~/types/course";
+import { PrismaClient } from "@prisma/client";
 
-export default defineEventHandler((event): LessonWithPath => {
-  const { chapterSlug, lessonSlug } = getRouterParams(event);
+const prisma = new PrismaClient();
 
-  const chapter: Maybe<Chapter> = course.chapters.find((chapter) => chapter.slug === chapterSlug);
-  if (!chapter) {
-    throw createError({
-      statusCode: 404,
-      message: "Chapter not found",
-    });
-  }
-  const lesson: Maybe<Lesson> = chapter.lessons.find((lesson) => lesson.slug === lessonSlug);
-  if (!lesson) {
-    throw createError({
-      statusCode: 404,
-      message: "Lesson not found",
-    });
-  }
-  return { ...lesson, path: `/course/chapter/${ chapter.slug }/lesson/${ lesson.slug }` };
+export default defineEventHandler(async (event) => {
+  const { chapterSlug, lessonSlug } = event.context.params as { chapterSlug: string, lessonSlug: string };
+  return prisma.lesson.findFirst({
+    where: {
+      Chapter: {
+        slug: chapterSlug,
+      },
+    },
+    include: {
+      Chapter: true,
+    },
+  });
 });
