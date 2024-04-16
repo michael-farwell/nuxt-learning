d6e8961a82921e0bdfae1114b283858730090181
Michael Farwell <whitewolf9909@gmail.com>
2024-04-16 06:26:24 -0400

    complete 7-11

--- server/api/stripe/paymentIntent.post.ts
+++ server/api/stripe/paymentIntent.post.ts
@@ -1,13 +1,36 @@
+import { PrismaClient } from "@prisma/client";
 import stripe from "~/server/api/stripe/stripe";
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   const { email } = await readBody(event);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 97 * 100,
-    currency: "usd",
-    metadata: {
-      email,
-    },
-  });
+  let paymentIntent;
+  try {
+    paymentIntent = await stripe.paymentIntents.create({
+      amount: 97 * 100,
+      currency: "usd",
+      metadata: { email },
+    });
+  } catch (e) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Error creating payment intent",
+    });
+  }
+  try {
+    await prisma.coursePurchase.create({
+      data: {
+        userEmail: email,
+        courseId: 1,
+        paymentId: paymentIntent.id,
+      },
+    });
+  } catch (e) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Error creating course purchase",
+    });
+  }
   return paymentIntent.client_secret;
 });
